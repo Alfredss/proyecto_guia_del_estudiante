2c53399124810f597684e9793f722a302f88ad3a
Alfredss <alfredss.24.7@gmail.com>
2016-11-26 12:16:38 -0500

    Creamos la ventana Login y tambien la funcionalidad del Sign In agregamos las dependencias de los .jade

--- app.js
+++ app.js
@@ -1,88 +1,89 @@
 //IMPORTACIONES
 var express = require("express");
 var app = express();
-
+var bodyParser = require("body-parser");
 //IMPORTACIONES MODELOS
-
-//var Usuario = require("./models/usuario.js").Usuario;
+var Usuario = require("./models/usuario.js").Usuario;
 //var Estudiante = require("./models/estudiante.js").Estudiante;
 var Curso = require("./models/curso.js").Curso;
-
-
-app.set("view engine", "jade");	//inicializamos el motor de vista
-app.use(express.static("public"));	//habilitamos la carpeta public para que almacene recursos
-
-
-
-
-var state = '';
+app.set("view engine", "jade"); //inicializamos el motor de vista
+app.use(express.static("public")); //habilitamos la carpeta public para que almacene recursos
+app.use(bodyParser.json()); //para peticiones con el formato application/json
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 //METODOS GET Y SET
-app.get("/", function(req, res){
-	/*var curso = new Curso({
-		sigla: "ec142",
-		nombre: "Microeconomía",
-		credito: 3,
-		horas_teoricas: 2,
-		horas_practica: 2,
-		horas_laboratorio: 0,
-		requisito: "ec141",
-		departamento: "Ciencias Económicas y Administrativas",
-		estado: "desaprobado", 	// aprobado / desaprobado
-		tipo_curso: "bajo",  // curso llave
-		serie: "100",
-		siglo: 2
-	});
-
-	curso.save(function(){
-		console.log("curso guardado");
-	});*/
-	/*
-	var user = new Usuario({
-		codigo_estudiante: 12345678,
-		nombre_usuario: "admin2",
-		password : "1234567",
-	});
-	
-	user.save(function(){
-		console.log("datos guardados correctamente");
-	});
-	*/
-	
-	Curso.findOne({sigla:'ec142'},"estado",function(err, data){
-		state = data.estado;
-		//console.log(data);
-		//console.log(state);
-		module.exports = state;
-		res.render("index", {hola:state});
-		//module.exports.state=state;
-		
-	});
-
-	
-	
-})//acceso a principal
-
-
-app.get("/estadisticas", function(req, res){
-	res.render("estadisticas");	
-	console.log(state);
-})//acceso a estadisticas
-
-
-app.get("/blog", function(req, res){
-	res.render("blog");
-})//acceso a blog
-
-
-app.get("/informacion", function(req, res){
-	res.render("informacion");
-})//acceso a informacion
-
-
-app.get("/configuracion", function(req, res){
-	res.render("configuracion");
-})//acceso a configuracion
-
-
-
+app.get("/", function(req, res) {
+        res.render("inicio");
+        /*
+        var user = new Usuario({
+            codigo_estudiante: 12345678,
+            nombre_usuario: "admin2",
+            password : "1234567",
+        });
+    
+        user.save(function(){
+            console.log("datos guardados correctamente");
+        });
+        */
+        /*  
+            Curso.find({},"sigla",function(err, data){
+                state = data[1];
+                //console.log(data);
+                //console.log(state);
+                //res.render("index", {hola:state});        
+                //console.log(data);
+                console.log(state);
+                res.render("index");
+            });
+        */
+    }) //acceso a principal
+app.get("/index", function(req, res) {
+        Curso.find({}, {
+            sigla: 1,
+            nombre: 1,
+            estado: 1,
+            requisito: 1
+        }, function(err, data) {
+            res.render("index", {
+                curso: data
+            });
+        });
+    }) //acceso a cuenta principal
+app.post("/login", function(req, res) {
+        Usuario.find({
+            "nombre_usuario": req.body.user,
+            "password": req.body.password
+        }, function(err, data) {
+            if (data.length > 0) {
+                Curso.find({}, {
+                    sigla: 1,
+                    nombre: 1,
+                    estado: 1,
+                    requisito: 1
+                }, function(err, data) {
+                    res.render("index", {
+                        curso: data
+                    });
+                });
+            } else {
+                res.render("inicio");
+            }
+        });
+    }) //post login
+app.get("/estadisticas", function(req, res) {
+        res.render("estadisticas");
+    }) //acceso a estadisticas
+app.get("/blog", function(req, res) {
+        res.render("blog_home");
+    }) //acceso a blog
+app.get("/blog_post", function(req, res) {
+        res.render("blog_post");
+    }) //acceso a blog
+app.get("/informacion", function(req, res) {
+        res.render("informacion");
+    }) //acceso a informacion
+app.get("/configuracion", function(req, res) {
+        res.render("configuracion");
+    }) //acceso a configuracion
 app.listen(3305);
--- models/usuario.js
+++ models/usuario.js
@@ -1,27 +1,15 @@
 var mongoose = require('mongoose'); //ORM
-
-var Schema = mongoose.Schema;	//modulo esquema
-
+var Schema = mongoose.Schema; //modulo esquema
 mongoose.connect("mongodb://localhost/guia_estudiante_db"); //conexion con mongodb
-
-
 var usuarioJSON = {
-	codigo_estudiante: Number,
-	nombre_usuario: String,
-	password : String
-};//creamos la estructura del esquema
-
-var schema_usuario = Schema(usuarioJSON);//creamos el esquema
-
-
-var Usuario = mongoose.model("Usuario", schema_usuario);//creamos el modelo
-
-
-module.exports.Usuario = Usuario;//exportamos
-
-
+    codigo_estudiante: Number,
+    nombre_usuario: String,
+    password: String
+}; //creamos la estructura del esquema
+var schema_usuario = Schema(usuarioJSON); //creamos el esquema
+var Usuario = mongoose.model("Usuario", schema_usuario); //creamos el modelo
+module.exports.Usuario = Usuario; //exportamos
 /*
 	coleccion : tabla
 	documentos : registros
 */
-
